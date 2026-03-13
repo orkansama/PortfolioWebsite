2ca380fe634e5c87eea7ec333a0837fa6cb7d134
OrkanGoekcen <orkan.gok@gmail.com>
2026-03-14 00:26:22 +0100

    added array from type IconWrapperProperties to src/app/page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,26 @@
 import Layout from '../app/layout'
+import { IconWrapperProperties } from '../types/IconWrapperProperties'
+import { FaGithub } from "react-icons/fa";
+import { CiLinkedin } from "react-icons/ci";
 
-export default function Page() {
+let globalIconSize: number = 30
+let githubIcon: IconWrapperProperties = {
+  icon: FaGithub,
+  name: "githubIcon",
+  size: globalIconSize 
+}
 
+let linkedinIcon: IconWrapperProperties = {
+  icon: CiLinkedin,
+  name: "linkedinIcon",
+  size: globalIconSize 
+}
+
+export default function MainPage() {
+  let iconsArray: IconWrapperProperties[] = [githubIcon, linkedinIcon];
   return (
     <Layout>
       <div>Test</div>
     </Layout>
-
   )
 }
