d38bd05046891708e5fe113a79d56544cb8b7498
OrkanGoekcen <orkan.gok@gmail.com>
2026-03-16 16:30:03 +0100

    navbar.tsx is now properly used in src/app/page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,12 +4,12 @@ import { CiLinkedin } from "react-icons/ci";
 import { Navbar } from '@/components/Navbar/Navbar';
 
 export default function MainPage() {
-  let iconsArray: React.ComponentType<React.SVGProps<SVGSVGElement>>[] = [FaGithub, CiLinkedin];
-  const pageSectionNames: string[] = ["Landingpage", "About", "Skills", "Projects", "Contact"];
-  return (
-    <Layout>
+	let iconsArray: React.ComponentType<React.SVGProps<SVGSVGElement>>[] = [FaGithub, CiLinkedin];
+	const pageSectionNames: string[] = ["Landingpage", "About", "Skills", "Projects", "Contact"];
 
-    <Navbar{pageSectionNames, iconsArray} />
-    </Layout>
-  )
+	return (
+		<Layout>
+			<Navbar pageSectionNames={pageSectionNames} iconsArray={iconsArray} />
+		</Layout>
+	)
 }
