737894d9d7d3fb886f9ad21b96f5407f3441aee2
OrkanGoekcen <orkan.gok@gmail.com>
2026-03-16 19:18:27 +0100

    added a new icon to iconsArray and landing page component in src/app/page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,14 +2,17 @@ import Layout from '../app/layout'
 import { FaGithub } from "react-icons/fa";
 import { CiLinkedin } from "react-icons/ci";
 import { Navbar } from '@/components/Navbar/Navbar';
+import { Landingpage } from '@/components/Landingpage/Landingpage';
+import { CiLocationOn } from "react-icons/ci";
 
 export default function MainPage() {
-	let iconsArray: React.ComponentType<React.SVGProps<SVGSVGElement>>[] = [FaGithub, CiLinkedin];
+	let iconsArray: React.ComponentType<React.SVGProps<SVGSVGElement>>[] = [FaGithub, CiLinkedin, CiLocationOn];
 	const pageSectionNames: string[] = ["Landingpage", "About", "Skills", "Projects", "Contact"];
 
 	return (
 		<Layout>
 			<Navbar pageSectionNames={pageSectionNames} iconsArray={iconsArray} />
+			<Landingpage iconsArray={iconsArray} />
 		</Layout>
 	)
 }
