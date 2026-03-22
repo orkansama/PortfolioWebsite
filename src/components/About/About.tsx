cbf4902bc50881eb38da87a0e1d949dda7921892
OrkanGoekcen <orkan.gok@gmail.com>
2026-03-22 12:20:10 +0100

    Added placeholder to About.tsx

---
+++ src/components/About/About.tsx
@@ -0,0 +1,32 @@
+import { PersonalClassElement } from "../PersonalClassElement/PersonalClassElement";
+import './style/aboutStyle.css'
+
+export function About() {
+    return (
+        <div id='about'>
+            <div>
+                <PersonalClassElement />
+            </div>
+            <div>
+                <p className="aboutMeText">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.<br></br>
+                    Aliquam libero, ut sint in minima corrupti placeat ex amet,<br></br>
+                    voluptatibus necessitatibus quam magni dolorem quisquam vero<br></br>
+                    aperiam nemo eligendi nesciunt impedit.<br></br>
+                    <br></br>
+                    <br></br>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.<br></br>
+                    Aliquam libero, ut sint in minima corrupti placeat ex amet,<br></br>
+                    voluptatibus necessitatibus quam magni dolorem quisquam vero<br></br>
+                    aperiam nemo eligendi nesciunt impedit.<br></br>
+                    <br></br>
+                    <br></br>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.<br></br>
+                    Aliquam libero, ut sint in minima corrupti placeat ex amet,<br></br>
+                    voluptatibus necessitatibus quam magni dolorem quisquam vero<br></br>
+                    aperiam nemo eligendi nesciunt impedit.<br></br>
+                </p>
+            </div>
+        </div>
+    )
+}
